3dfeba22edd33709c96b8d2800554b32fdea5816
刘瀚雄 <165266807+Gavin-LHX@users.noreply.github.com>
2026-02-10 01:54:27 +0800

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import { App } from './App.tsx' // 确保这里有花括号，且对应刚才导出的 export function App
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
